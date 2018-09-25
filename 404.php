555a47c68a41d1ae6a3e903a1d771414609b349c
Mehdi <mehdi.java.programmer@gmail.com>
2018-09-25 13:10:02 +0330

    second

---
+++ 404.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mehdi
+ * Date: 9/24/2018
+ * Time: 3:03 PM
+ */
--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
-echo "hello world";
+function wxyz($new_user, $second)
+{
+    echo "<br/>wxyz {$second}{$new_user}";
+}
+
+
+add_action('mbuss', 'wxyz', 9, 2);
+
+do_action('mbuss', "Mehdi ", 135);
